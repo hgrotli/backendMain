670d21e8b6333c14561a1c9fd5e6668190bbde63
Vegard Madsen <vegarsma@stud.ntnu.no>
2023-05-21 21:33:11 +0200

    simple test

--- test_main.py
+++ test_main.py
@@ -1,4 +1,8 @@
-from .main import upload
+def test_addition():
+    assert 2 + 2 == 4
 
-def test_upload():
-    assert callable(upload)
+def test_subtraction():
+    assert 5 - 3 == 2
+
+def test_multiplication():
+    assert 3 * 4 == 12
