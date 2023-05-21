b83b4558a2d0acf5cfbcf6d4ee5a2100ed987ec2
Vegard Madsen <vegarsma@stud.ntnu.no>
2023-05-21 20:15:43 +0200

    new stuff

--- test_main.py
+++ test_main.py
@@ -1,8 +1,59 @@
-def test_addition():
-    assert 2 + 2 == 4
+from flask import Flask, request, render_template
+from flask import jsonify
+import vobject
+import json
+from pymongo import MongoClient
+import pymongo
+import certifi
+from bson import json_util
+from flask import Response
+from bson import json_util
+from flask_cors import CORS
+import os
+import requests
 
-def test_subtraction():
-    assert 5 - 3 == 2
 
-def test_multiplication():
-    assert 3 * 4 == 12
+
+def upload():
+    if request.method == "POST":
+        file = request.files["file"]
+        file_content = file.read().decode("utf-8")
+        text = vobject.readComponents(file_content)
+        for dataContact in text:
+            phone = "--No Information--"
+            try:
+                phone = str(dataContact.tel.value)
+            except AttributeError:
+                pass
+            address = "--No Information--"
+            try:
+                address = str(dataContact.adr.value)
+            except AttributeError:
+                pass
+            company = "--No Information--"
+            try:
+                company = str(dataContact.org.value[0])
+            except AttributeError:
+                pass
+
+            MyContact = {
+                "email": dataContact.email.value,
+                "name": str(dataContact.n.value),
+                "fullname": dataContact.fn.value,
+                "address": address,
+                "company": company,
+                "phone": phone,
+            }
+
+            result = mycol.insert_one(MyContact)
+            MyData.append(str(result.inserted_id))
+            print(MyContact)
+
+            # Trigger an update request to the cache server
+        cache_url = "https://asi2-cache2.onrender.com/update_cache"
+        response = requests.get(cache_url)
+        return jsonify({"message": MyData})
+
+    else:
+        # show the upload form
+        return render_template("upload.html")
